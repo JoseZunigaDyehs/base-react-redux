e5ea7e7f47b0d358b3811bdcfab4b9f6eb8ae7a8
JoseZunigaDyehs <zuniga.joseignacio@gmail.com>
2018-04-24 12:07:25 -0300

    BASE REACT REDUX

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,31 +4,10 @@ import { firebaseApp } from '../firebaseApp'
 const Home = () => {
   return (
     <div>
-      <div class="form-group">
-        <label>Email address
-          <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
-        </label>
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      </div>
-      <div class="form-group">
-        <label>Password
-          <input type="password" class="form-control" id="pass" placeholder="Password" />
-        </label>
-      </div>
-      <button type="submit" class="btn btn-primary" onClick={(e) => login()}>Submit</button>
+    Home
     </div>
   )
 }
 
-const login = () => {
-  let email = document.getElementById('email').value;
-  let pass = document.getElementById('pass').value;
-  debugger
-  firebaseApp.auth().createUserWithEmailAndPassword(email, pass)
-    .catch(res => {
-      console.log(res.message)
-    })
-
-}
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -4,15 +4,6 @@ import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
 import store from './data/store'
-import { firebaseApp } from './firebaseApp'
-
-firebaseApp.auth().onAuthStateChanged(user => {
-  if(user){
-    console.log('user',user)
-  }else{
-    console.log('no conexion')
-  }
-})
 
 ReactDOM.render(
   <Provider store={store}>
